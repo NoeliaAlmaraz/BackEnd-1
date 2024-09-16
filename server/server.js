3af88a006bb0069f900cdb64969d497bcc0c3b15
NoeliaAlmaraz <156339696+NoeliaAlmaraz@users.noreply.github.com>
2024-09-16 21:21:53 +0200

    update, delete y cambio a carpeta de controllers (#1)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from "express";
-import productsManager from "./src/data/products.manager.js";
+import {
+  getAllProducts,
+  create,
+  getOneProduct,
+  update,
+  destroyProduct,
+} from "./src/controllers/products.controllers.js";
 
 try {
   const server = express();
@@ -10,13 +16,23 @@ try {
   //  la lectura de datos complejos en la url(parametros y consultas)
   server.use(express.urlencoded({ extended: true }));
 
+  //abliga al servidor a usar la funcionalidad de recepcion y emision del formato json para poder
+  //recibir objetos en la propiedad req.body y ademas se aplica json en toda la aplicacion
+  server.use(express.json());
+
   server.listen(port, ready);
 
   // definir una ruta para leer datos .get
   server.get("/", index);
-  server.get("/products", getAllProducts);
-  server.get("/products/:title/:price/:stock", create);
-  server.get("/products/:pid", getOneProduct);
+  server.get("/api/products", getAllProducts);
+  // para crear .post
+  server.post("/api/products", create);
+  //server.get("/products/:title/:price/:stock", create);
+  server.get("/api/products/:pid", getOneProduct);
+  // para actualizar .put
+  server.put("/api/products/:pid", update);
+  // para borrar .delete
+  server.delete("/api/products/:pid", destroyProduct);
 } catch (error) {
   console.log(error);
 }
@@ -28,76 +44,3 @@ function index(req, res) {
     return res.status(500).json({ mensaje: "Error" });
   }
 }
-
-async function getAllProducts(req, res) {
-  try {
-    //con esto condicionamos si existe la categoria, en ese caso se devuelve solo los productos con esa categoria
-    let { category } = req.query;
-    let products;
-    if (category) {
-      products = await productsManager.readAll(category);
-    } else {
-      products = await productsManager.readAll();
-    }
-    if (products.length > 0) {
-      return res
-        .status(200)
-        .json({ mensaje: "Productos operativos", products });
-    } else {
-      return res
-        .status(404)
-        .json({ mensaje: "No hay productos con esa categoría" });
-    }
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error" });
-  }
-}
-
-//esto no está bien ya que no se suele crear un producto por la url, esto se utiliza para leer y traer datos para consultas
-async function create(req, res) {
-  try {
-    const { title, price, stock } = req.params;
-    //en este caso es let porque si no existe, modificamos la variable
-    let { category, supplier } = req.query;
-    //se condiciona para que si no existe, se cree pero con el valor none ya que es un valor opcional por query
-    if (!category) {
-      category = "none";
-    }
-    if (!supplier) {
-      supplier = "none";
-    }
-    const response = await productsManager.create({
-      title,
-      price,
-      stock,
-      category,
-      supplier,
-    });
-    return res.status(201).json({ mensaje: "Producto creado", response });
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error" });
-  }
-}
-
-async function getOneProduct(req, res) {
-  try {
-    const { pid } = req.params;
-    const product = await productsManager.read(pid);
-    if (product) {
-      return res.status(200).json({ mensaje: "Producto operativo", product });
-    } else {
-      return res.status(404).json({ mensaje: "Producto no encontrado" });
-    }
-  } catch (error) {
-    return res.status(500).json({ mensaje: "Error" });
-  }
-}
-
-// para crear .post
-//server.post()
-
-// para actualizar .put
-//server.put()
-
-// para borrar .delete
-//server.delete()
